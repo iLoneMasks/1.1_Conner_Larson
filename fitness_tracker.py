20dee615b8146e39236833569e21efe0e7a5986a
iLoneMasks <connerl.23079@obhs.school.nz>
2025-08-25 12:16:19 +1200

    added multiline description

---
+++ fitness_tracker.py
@@ -0,0 +1,13 @@
+'''
+    author: Conner Larson-Snow
+    date: 25/08/25
+    version: 1
+    description: This code is to collect data from the user and store the values in a list
+'''
+#--------libraries---------------
+
+#--------functions---------------
+
+#-----main-----------------------
+if(__name__== "__main__"):
+    main():
