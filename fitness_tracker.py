d8f2215164abfdec6c35302d59a6607b70f756c9
iLoneMasks <connerl.23079@obhs.school.nz>
2025-08-25 12:27:39 +1200

    added name input

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -5,9 +5,10 @@
     description: This code is to collect data from the user and store the values in a list
 '''
 #--------libraries---------------
-
+import random
 #--------functions---------------
+print input('What is your name?: ')
 
 #-----main-----------------------
 if(__name__== "__main__"):
-    main():
+    main()
