08c891cbea76dae6983b8f1fb04438df1be86b3c
iLoneMasks <connerl.23079@obhs.school.nz>
2025-08-26 14:39:49 +1200

    added append for all inputs

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -7,11 +7,17 @@
 #--------libraries---------------
 import random
 #--------functions---------------
-run_times = []
+def main():
+    name = ''
+    age = ''
+    run_times = []
 
-name = input('What is your name?: ')
-age = int(input('What is your age?: '))
-run_time = input('What was your completed running time in minutes?: ')
+    name = input('What is your name?: ')
+    run_times.append(name)
+    age = int(input('What is your age?: '))
+    run_times.append(age)
+    run_time = input('What was your completed running time in minutes?: ')
+    run_times.append(run_time)
 #-----main-----------------------
 if(__name__== "__main__"):
     main()
