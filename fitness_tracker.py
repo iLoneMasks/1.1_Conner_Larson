703a1c71fe012d681baeee2292daccb675ff51d3
iLoneMasks <connerl.23079@obhs.school.nz>
2025-08-28 09:07:46 +1200

    added loop to run time

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -10,14 +10,16 @@ import random
 def main():
     name = ''
     age = ''
-    run_times = []
+    run_times_list = []
+    run_times_inputs = 5
 
     name = input('What is your name?: ')
-    run_times.append(name)
+    name.append(name)
     age = int(input('What is your age?: '))
-    run_times.append(age)
-    run_time = input('What was your completed running time in minutes?: ')
-    run_times.append(run_time)
+    age.append(age)
+    for i in range(run_times_inputs):
+        run_times = input(f"Please enter your run time: ")
+        run_times_list.append(run_times)
 #-----main-----------------------
 if(__name__== "__main__"):
     main()
