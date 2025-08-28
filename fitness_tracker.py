40d6d6df3e0b92218981b46a5d61fa9b01643b18
iLoneMasks <connerl.23079@obhs.school.nz>
2025-08-29 10:31:25 +1200

    added print codes for all inputs and fixed up errors

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -18,13 +18,14 @@ def main():
     while True:
         try:
             age = int(input('How old are you?: '))
-            if age > 13:
+            if age >= 14:
                 break
             else:
-                print("Your age is invalid. You must be older than 13.")
-            except ValueError
-                print("Please input a valid number for your age")
-            
+                print("Your age is invalid. You must be 14 or older.")
+        except ValueError:
+                print("Please input a valid number for your age.")
+
+
     for i in range(run_times_inputs):
         try:
             run_times = float(input(f"Please enter your run time #{i+1}: "))
@@ -32,7 +33,15 @@ def main():
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
             continue
-        
+
+        average_run_time = sum(run_times_list) / len(run_times_list)
+
+        print("\nSummary: ")
+        print(f"Name: {name}")
+        print(f"Age: {age}")
+        print("Run Times:", run_times_list)
+        print(f"Average Run Time: {average_run_time:.2f}")
+
 #-----main-----------------------
 if(__name__== "__main__"):
     main()
